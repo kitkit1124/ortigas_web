ab223dd6db921a8762f4d71f3d826a78da4bdb98
Gutz <gutzby.marzan@digify.com.ph>
2019-02-06 16:45:55 +0800

    email17

--- app/modules/messages/controllers/Messages.php
+++ app/modules/messages/controllers/Messages.php
@@ -152,9 +152,10 @@ class Messages extends MX_Controller {
             $this->email->set_newline("\r\n");
             $this->email->to($this->config->item('app_email'));
             $this->email->from($this->input->post('website_email'), $this->config->item('website_name'));
-            $this->email->subject('Inquiry for '.$this->input->post('message_section').' - '.$this->input->post('message_section_id'));
+            //$this->email->subject('Inquiry for '.$this->input->post('message_section').' - '.$this->input->post('message_section_id'));
+            $this->email->subject('Inquiry');
             $this->email->set_mailtype("html");
-            $this->email->message($this->input->post('message_content'));
+            $this->email->message('test content');
             $this->email->send();
 
             pr($this->email->print_debugger(array('headers')));
