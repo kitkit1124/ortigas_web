416f73cae8a719f9a3214270c1087c0cbf67d3e2
Gutz <gutzby.marzan@digify.com.ph>
2019-02-06 15:54:34 +0800

    email13

--- app/modules/messages/controllers/Messages.php
+++ app/modules/messages/controllers/Messages.php
@@ -150,11 +150,11 @@ class Messages extends MX_Controller {
 
             $this->email->clear();
             $this->email->set_newline("\r\n");
-            $this->email->to('gutzby.nobleza.marzan@gmail.com');
-            $this->email->from($this->config->item('website_email'), $this->config->item('website_name'));
-            $this->email->subject('');
+            $this->email->to($this->config->item('application_email'));
+            $this->email->from($this->input->post('message_email'), $this->config->item('website_name'));
+            $this->email->subject('Inquiry for '$this->input->post('message_section'). - .$this->input->post('message_section_id'));
             $this->email->set_mailtype("html");
-            $this->email->message('Test Content, Vape muna ako 3:28');
+            $this->email->message($this->input->post('message_content'));
             $this->email->send();
 
             pr($this->email->print_debugger(array('headers')));
