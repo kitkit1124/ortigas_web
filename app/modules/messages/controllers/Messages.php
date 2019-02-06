e2b54a1098b851f5c7ef0184ea6c14f85c81e344
Gutz <gutzby.marzan@digify.com.ph>
2019-02-06 14:46:40 +0800

    email11

--- app/modules/messages/controllers/Messages.php
+++ app/modules/messages/controllers/Messages.php
@@ -154,7 +154,7 @@ class Messages extends MX_Controller {
             $this->email->from('no-reply@test.com');
             $this->email->subject('Subject');
             $this->email->set_mailtype("html");
-            $this->email->message('Test Content, Vape muna ako'.date_now());
+            $this->email->message('Test Content, Vape muna ako '.date("h:s A"));
             $this->email->send();
 
             pr($this->email->print_debugger(array('headers')));
