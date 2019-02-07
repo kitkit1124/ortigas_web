fef82564cc2e118b4379fb080e6e1927f4cc5e3f
Gutz <gutzby.marzan@digify.com.ph>
2019-02-07 14:32:10 +0800

    email22

--- app/modules/messages/views/messages_email.php
+++ app/modules/messages/views/messages_email.php
@@ -1,47 +1,81 @@
 <html>
 <head>
 <link rel="stylesheet" type="text/css" href="<?php echo site_url('npm/bootstrap/css/bootstrap.min.css'); ?>">
-<?php $this->template->add_css(module_css('message', 'email_message'), 'embed'); ?>
+<style type="text/css">
+
+	body{
+		font-family: Helvetica;
+	}
+	.label{
+		color: #07793F;
+		text-transform: uppercase;
+		font-weight: bold;
+		letter-spacing: -1px;
+		width: 200px;
+		vertical-align: top;
+	}
+	.label span{
+		float: right;
+		text-align: right;
+	}
+	td.eval {
+		max-width: 600px;
+	}
+	td{
+		margin: 10px;
+		padding: 10px;
+		display: inline-block;
+		font-family: Helvetica;
+	}
+	h1{
+		color: #07793F;
+		margin-left: 10px;
+	}
+	table{
+		margin-left: 10px;
+	}
+</style>
 </head>
 <body>
-<div class="content">
-	<div class="row">
-		<div class="col-sm-3">Inquirty Type:</div>
-		<div class="col-sm-6"><?php echo $message_section; ?></div>
-	</div>
-	<div class="row">
-		<div class="col-sm-3">Subject</div>
-		<div class="col-sm-6"><?php echo $message_section_id; ?></div>
-	</div>
-	<div class="row">
-		<div class="col-sm-3">Name:</div>
-		<div class="col-sm-6"><?php echo $message_name; ?></div>
-	</div>
-	<div class="row">
-		<div class="col-sm-3">Email:</div>
-		<div class="col-sm-6"><?php echo $message_email; ?></div>
-	</div>
+	<h1>Inquiry Notification</h1>
+	<table border="0">
+		<tr>
+			<td class="label">Inquiry Type<span>:</span></td>
+			<td class="eval"><?php echo $message_section; ?></td>
+		</tr>
+		<tr>
+			<td class="label">Subject<span>:</span></td>
+			<td class="eval"><?php echo $message_section_id; ?></td>
+		</tr>
+		<tr>
+			<td class="label">Name<span>:</span></td>
+			<td class="eval"><?php echo $message_name; ?></td>
+		</tr>
+		<tr>
+			<td class="label">Email<span>:</span></td>
+			<td class="eval"><?php echo $message_email; ?></td>
+		</tr>
 
-	<?php if(isset($message_mobile) && $message_mobile): ?>
-	<div class="row">
-		<div class="col-sm-3">Mobile Number:</div>
-		<div class="col-sm-6"><?php echo $message_mobile; ?></div>
-	</div>
-	<?php endif; ?>
+		<?php if(isset($message_mobile) && $message_mobile): ?>
+		<tr>
+			<td class="label">Mobile Number<span>:</span></td>
+			<td class="eval"><?php echo $message_mobile; ?></td>
+		</tr>
+		<?php endif; ?>
 
-	<?php if(isset($message_location) && $message_location): ?>
-		<div class="row">
-			<div class="col-sm-3">Location:</div>
-			<div class="col-sm-6"><?php echo $message_location; ?></div>
-		</div>
-	<?php endif; ?>
+		<?php if(isset($message_location) && $message_location): ?>
+			<tr>
+				<td class="label">Location<span>:</span></td>
+				<td class="eval"><?php echo $message_location; ?></td>
+			</tr>
+		<?php endif; ?>
 
-	<?php if(isset($message_content) && $message_content): ?>
-		<div class="row">
-			<div class="col-sm-3">Message:</div>
-			<div class="col-sm-6"><?php echo $message_content; ?></div>
-		</div>
-	<?php endif; ?>
-</div>
+		<?php if(isset($message_content) && $message_content): ?>
+			<tr>
+				<td class="label">Message<span>:</span></td>
+				<td class="eval"><?php echo $message_content; ?></td>
+			</tr>
+		<?php endif; ?>
+	</table>
 </body>
 </html>
